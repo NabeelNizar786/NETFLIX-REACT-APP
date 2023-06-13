e9bc470f1173fdb098037eeb132f79ae9b8ffd86
Nabeel <nabeelnizar786@gmail.com>
2023-06-13 17:10:42 +0530

    secondCommit

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {API_KEY, imageUrl} from '../../Constants/constants'
+import axios from '../../axios';
 import './Banner.css';
 
 function Banner() {
+  const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`)
+    .then((Response)=>{
+      const movies = Response.data.results;
+      const randomIndex = Math.floor(Math.random() * movies.length);
+      console.log(movies[randomIndex]);
+      setMovie(movies[randomIndex]);
+    })
+  }, [])
+  
   return (
-    <div className='Banner'>
+    <div style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}} className='Banner'>
       <div className='content'>
-        <h1 className='title'>Movie Name</h1>
+        <h1 className='title'>{movie ? movie.title : ""}</h1>
         <div className='banner_buttons'>
           <button className='button'>Play</button>
           <button className='button'>My list</button>
         </div>
-        <h1 className='description'>Generate Lorem Ipsum placeholder text for use in your graphic, print and web layouts.</h1>
+        <h1 className='description'>{movie ? movie.overview : ""}</h1>
       </div>
       <div className="fade_bottom"></div>
     </div>
